4f08ecc6d7a88aeaebcc79fad7e7b902ebe726cd
Cassie <cassie.lee1023@gmail.com>
2024-05-30 19:29:05 +0100

    started structuring nav bar

--- client/src/subcomponents/BottomNavbar.jsx
+++ client/src/subcomponents/BottomNavbar.jsx
@@ -1,3 +1,7 @@
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+
 export default function BottomNavbar() {
   return (
     <>
--- client/src/subcomponents/TopNavbar.jsx
+++ client/src/subcomponents/TopNavbar.jsx
@@ -1,6 +1,23 @@
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+
 export default function TopNavbar() {
+
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+
+  }, [location])
+
   return (
     <>
+      <nav className='topnav'>
+        <ul>
+
+        </ul>
+      </nav>
     </>
   )
 }

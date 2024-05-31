fe105d0d8d08f6008a74855656be17ee07931928
Cassie <cassie.lee1023@gmail.com>
2024-05-31 09:45:24 +0100

    imported pakages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,10 @@
-
+import axios from 'axios'
+import { setToken } from '../../lib/auth'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+
+
   return (
     <>
       <h1>Login here...</h1>

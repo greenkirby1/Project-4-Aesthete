f4a4a7a94b0a4c1da60224155cca7c81d1e22187
Cassie <cassie.lee1023@gmail.com>
2024-06-03 11:51:52 +0100

    solved access token

--- users/serializers/common.py
+++ users/serializers/common.py
@@ -13,6 +13,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
   password = serializers.CharField(write_only=True)
   password_confirmation = serializers.CharField(write_only=True)
+  access = serializers.CharField(read_only=True)
 
   class Meta:
     model = User
@@ -23,6 +24,7 @@ class RegisterSerializer(serializers.ModelSerializer):
       'password', 
       'password_confirmation', 
       'is_artist',
+      'access'
     )
 
   def validate(self, data):
@@ -43,8 +45,9 @@ class RegisterSerializer(serializers.ModelSerializer):
     user_data = UserRegisterDetailSerializer(new_user).data
     get_token = RefreshToken.for_user(new_user)
     # print(get_token, get_token.access_token)
-    token_data = {'refresh': str(get_token), 'access': str(get_token.access_token)}
-    user_data.update(token_data)
+    # token_data = {'refresh': str(get_token), 'access': str(get_token.access_token)}
+    # user_data.update(token_data)
+    user_data['access'] = str(get_token.access_token)
     print('RIGHT HERE ->', user_data)
 
     return user_data
